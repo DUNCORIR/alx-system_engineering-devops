373732aa571967808c36c668b15977380a49b9d9
duncorir <duncorir@mail.com>
2025-01-11 13:14:20 +0300

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# This Puppet manifest installs the Flask package (version 2.1.0) using pip3.
+# Puppet manifest to install Flask (v2.1.0) and Werkzeug (v2.1.0)
 
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip',
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
+}
+
+exec { 'install werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.0',
+  unless  => '/usr/bin/pip3 show werkzeug | grep "Version: 2.1.0"',
+  require => Exec['install flask'], # Ensures Flask is installed first
 }
