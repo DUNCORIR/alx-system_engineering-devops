d387ce262ed33a19045a4810e0eea3d43c74c259
duncorir <duncorir@mail.com>
2025-02-04 16:58:36 +0300

    header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,32 @@
 # This Puppet manifest configures Nginx with a custom HTTP response header "X-Served-By".
-
-node default {
+class nginx_custom_http_header {
   # Ensure Nginx is installed
   package { 'nginx':
     ensure => installed,
   }
 
-  # Ensure the Nginx service is enabled and running
+  # Ensure Nginx service is enabled and running
   service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    subscribe  => File['/etc/nginx/conf.d/custom_header.conf'], # Restart on config change
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
   }
 
-  # Custom Nginx configuration with the header
+  # Configure the default site for Nginx
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => template('nginx/default.erb'),
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  # Add the custom header to Nginx configuration
   file { '/etc/nginx/conf.d/custom_header.conf':
     ensure  => file,
-    content => template('nginx/custom_header.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
-
-  # Ensure the Nginx configuration is reloaded after changes
-  exec { 'reload-nginx':
-    command     => '/usr/sbin/nginx -s reload',
-    refreshonly => true,
-    subscribe   => File['/etc/nginx/conf.d/custom_header.conf'],
+    content => "add_header X-Served-By $hostname;\n",
+    notify  => Service['nginx'],
   }
 }
+
+# Apply the nginx_custom_http_header class
+include nginx_custom_http_header
