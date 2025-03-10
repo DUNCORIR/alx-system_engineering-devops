5d8736b4347ab23e4127f1de7fa73fa95d73262b
duncorir <duncorir@mail.com>
2025-03-10 19:51:50 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
-# Ensure Apache, PHP, and required modules are installed
-package { ['apache2', 'php', 'php-mysql', 'libapache2-mod-php', 'mysql-server', 'wget', 'tar']:
+# Puppet manifest to fix Apache 500 error and set up WordPress
+
+# Ensure Apache and PHP are installed
+package { ['apache2', 'php', 'php-mysql', 'libapache2-mod-php']:
   ensure => installed,
 }
 
@@ -10,60 +12,60 @@ service { 'apache2':
   require => Package['apache2'],
 }
 
-# Ensure MySQL service is running
-service { 'mysql':
-  ensure  => running,
-  enable  => true,
-  require => Package['mysql-server'],
-}
-
-# Download WordPress
+# Download and extract WordPress
 exec { 'download-wordpress':
   command => '/usr/bin/wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress.tar.gz',
   creates => '/tmp/wordpress.tar.gz',
-  user    => 'root',
 }
 
-# Extract WordPress
 exec { 'extract-wordpress':
   command => '/bin/tar -xzf /tmp/wordpress.tar.gz -C /var/www/html --strip-components=1',
   creates => '/var/www/html/index.php',
   require => Exec['download-wordpress'],
-  user    => 'root',
 }
 
-# Set correct ownership and permissions
+# Ensure WordPress directory has correct permissions
 file { '/var/www/html':
   ensure  => directory,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0755',
-  recurse => true,
+  recurse => true,  # Recursively apply ownership and permissions
   require => Exec['extract-wordpress'],
 }
 
-# Configure wp-config.php using a template
+# Configure wp-config.php
 file { '/var/www/html/wp-config.php':
   ensure  => file,
-  content => template('/home/duncorir/alx-system_engineering-devops/0x17-web_stack_debugging_3/templates/wp-config.php.erb'),
+  content => "<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+?>",
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
-  require => File['/var/www/html'],
+  require => Exec['extract-wordpress'],
 }
 
-# Fix Apache configuration if needed
+# Ensure Apache serves the WordPress site
 file { '/etc/apache2/sites-available/000-default.conf':
   ensure  => file,
-  content => template('/home/duncorir/alx-system_engineering-devops/0x17-web_stack_debugging_3/templates/apache-default.conf.erb'),
+  content => "<VirtualHost *:80>
+    DocumentRoot /var/www/html
+    <Directory /var/www/html>
+        AllowOverride All
+        Require all granted
+    </Directory>
+    ErrorLog /var/log/apache2/error.log
+    CustomLog /var/log/apache2/access.log combined
+</VirtualHost>",
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
   notify  => Service['apache2'],
-}
-
-# Restart Apache to apply changes
-exec { 'restart-apache':
-  command     => '/usr/bin/systemctl restart apache2',
-  refreshonly => true,
+  require => Package['apache2'],
 }
