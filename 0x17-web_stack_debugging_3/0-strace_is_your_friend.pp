de6dab348e258052126c6849c7aa924a17e79c22
duncorir <duncorir@mail.com>
2025-03-10 19:29:52 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
-# Puppet manifest to fix Apache 500 error and set up WordPress
-
-# Ensure Apache and PHP are installed
-package { ['apache2', 'php', 'php-mysql', 'libapache2-mod-php']:
+# Ensure Apache, PHP, and required modules are installed
+package { ['apache2', 'php', 'php-mysql', 'libapache2-mod-php', 'mysql-server', 'wget', 'tar']:
   ensure => installed,
 }
 
@@ -12,55 +10,60 @@ service { 'apache2':
   require => Package['apache2'],
 }
 
-# Ensure MySQL is installed and running
-package { 'mysql-server':
-  ensure => installed,
-}
-
+# Ensure MySQL service is running
 service { 'mysql':
   ensure  => running,
   enable  => true,
   require => Package['mysql-server'],
 }
 
-# Download and set up WordPress
+# Download WordPress
 exec { 'download-wordpress':
   command => '/usr/bin/wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress.tar.gz',
   creates => '/tmp/wordpress.tar.gz',
+  user    => 'root',
 }
 
+# Extract WordPress
 exec { 'extract-wordpress':
   command => '/bin/tar -xzf /tmp/wordpress.tar.gz -C /var/www/html --strip-components=1',
   creates => '/var/www/html/index.php',
-  require => [Exec['download-wordpress'], File['/var/www/html']],
+  require => Exec['download-wordpress'],
+  user    => 'root',
 }
 
-# Ensure WordPress directory has correct permissions
+# Set correct ownership and permissions
 file { '/var/www/html':
   ensure  => directory,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0755',
-  require => Package['apache2'],
-}
-
-# Configure wp-config.php
-file { '/var/www/html/wp-config.php':
-  ensure  => file,
-  content => template('/home/duncorir/alx-system_engineering-devops/0x17-web_stack_debugging_3/templates/wordpress/wp-config.php.erb'),
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
+  recurse => true,
   require => Exec['extract-wordpress'],
 }
 
-# Ensure Apache serves the WordPress site
+# Configure wp-config.php using a template
+file { '/var/www/html/wp-config.php':
+  ensure  => file,
+  content => template('/home/duncorir/alx-system_engineering-devops/0x17-web_stack_debugging_3/templates/wp-config.php.erb'),
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  require => File['/var/www/html'],
+}
+
+# Fix Apache configuration if needed
 file { '/etc/apache2/sites-available/000-default.conf':
   ensure  => file,
-  content => template('/home/duncorir/alx-system_engineering-devops/0x17-web_stack_debugging_3/templates/apache/000-default.conf.erb'),
+  content => template('/home/duncorir/alx-system_engineering-devops/0x17-web_stack_debugging_3/templates/apache-default.conf.erb'),
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
   notify  => Service['apache2'],
-  require => Package['apache2'],
+}
+
+# Restart Apache to apply changes
+exec { 'restart-apache':
+  command     => '/usr/bin/systemctl restart apache2',
+  refreshonly => true,
 }
